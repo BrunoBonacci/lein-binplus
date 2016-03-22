9fe419bec3a66f319dfaa6cb9d70642ca31fbed7
Bruno Bonacci <bruno.bonacci@gmail.com>
2016-03-22 11:34:01 +0000

    added scm project details

--- project.clj
+++ project.clj
@@ -2,8 +2,13 @@
   :description "A leiningen plugin for generating standalone console
   executables for your project."
   :url "https://github.com/BrunoBonacci/lein-binplus"
+
+  :scm {:name "github"
+        :url "https://github.com/BrunoBonacci/lein-binplus"}
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[me.raynes/fs "1.4.0"]]
   :eval-in-leiningen true
 
